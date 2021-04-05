f7cf26cb157f4603c802619202e9bbf6219a275f
x <522909322@qq.com>
2021-04-05 09:45:36 +0800

    leetcode

---
+++ src/main/java/easy/Solution88.java
@@ -0,0 +1,28 @@
+package easy;
+
+/**
+ * @author zhangjun
+ * @version 2021/4/5
+ */
+public class Solution88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = nums1.length;
+        --m;
+        --n;
+        while(m >= 0 && n >= 0){
+            if(nums1[m] > nums2[n]){
+                nums1[--index] = nums1[m--];
+            }else{
+                nums1[--index] = nums2[n--];
+            }
+        }
+
+        while(m >= 0){
+            nums1[--index] = nums1[m--];
+        }
+
+        while(n >= 0){
+            nums1[--index] = nums2[n--];
+        }
+    }
+}
--- src/main/java/list/ListNode.java
+++ src/main/java/list/ListNode.java
@@ -4,10 +4,14 @@ package list;
  * @author zhangjun
  * @version 2020/09/25  0:42
  */
+
 public class ListNode {
     public int val;
     public ListNode next;
     public ListNode() {};
     public ListNode(int x) { val = x; }
-    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
