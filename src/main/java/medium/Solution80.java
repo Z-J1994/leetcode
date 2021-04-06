11404f8bb85e5f765bb3373357f8d0631b22432e
x <522909322@qq.com>
2021-04-06 20:05:06 +0800

    leetcode

--- src/main/java/medium/Solution80.java
+++ src/main/java/medium/Solution80.java
@@ -5,4 +5,17 @@ package medium;
  * @version 2021/4/6
  */
 public class Solution80 {
+    public int removeDuplicates(int[] nums) {
+        int length = nums.length;
+        if(length <= 2){
+            return length;
+        }
+        int left = 1;
+        for(int right = 2;right < length;++right){
+            if(nums[left - 1] != nums[right]){
+                nums[++left] = nums[right];
+            }
+        }
+        return left + 1;
+    }
 }
