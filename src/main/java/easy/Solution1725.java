17d27e2b3042452642c882b0269fe9476bde95ea
x <522909322@qq.com>
2021-06-09 18:35:53 +0800

    leetcode

---
+++ src/main/java/easy/Solution1725.java
@@ -0,0 +1,23 @@
+package main.java.easy;
+
+/**
+ * @author zhangjun
+ * @version 2021/6/9
+ */
+public class Solution1725 {
+    public int countGoodRectangles(int[][] rectangles) {
+        int count = 0;
+        int maxLen = 0;
+        for(int r[] : rectangles){
+            int min = Math.min(r[0],r[1]);
+            if(min > maxLen){
+                count = 1;
+                maxLen = min;
+            }
+            else if(min == maxLen){
+                ++count;
+            }
+        }
+        return count;
+    }
+}
