ad0bec10d13c1c65a56e4ad13d5254c4c87246c1
x <522909322@qq.com>
2021-06-15 07:54:55 +0800

    update852

--- src/main/java/easy/Solution852.java
+++ src/main/java/easy/Solution852.java
@@ -5,11 +5,30 @@ package easy;
  * @version 2020/09/27  21:50
  */
 public class Solution852 {
+    //    public int peakIndexInMountainArray(int[] arr) {
+//        int index = 1;
+//        while(index < arr.length && arr[index] > arr[index - 1]){
+//            index++;
+//        }
+//        return index - 1;
+//    }
+
     public int peakIndexInMountainArray(int[] arr) {
-        int index = 1;
-        while(index < arr.length && arr[index] > arr[index - 1]){
-            index++;
-        }
-        return index - 1;
+        return binarySearch(arr, 0, arr.length - 1);
     }
+
+    private int binarySearch(int[] arr, int lo, int hi) {
+        if (hi - lo < 2) {
+            return -1;
+        }
+        int mid = (hi - lo) / 2 + lo;
+        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+            return mid;
+        }
+        int l = binarySearch(arr, lo, mid);
+        int r = binarySearch(arr, mid, hi);
+
+        return (l == -1) ? r : l;
+    }
+
 }
