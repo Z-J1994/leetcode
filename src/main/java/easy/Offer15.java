55933173265839e3021c8eb5b988bccb254ec3a8
x <522909322@qq.com>
2021-06-23 20:03:36 +0800

    update offer15

--- src/main/java/easy/Offer15.java
+++ src/main/java/easy/Offer15.java
@@ -5,14 +5,23 @@ package easy;
  * @version 2020/09/26  14:31
  */
 public class Offer15 {
+    //    public int hammingWeight(int n) {
+//        int count = 0;
+//        while(n != 0){
+//            if((n & 0x1) == 1){
+//                count++;
+//            }
+//            n >>>= 1;
+//        }
+//        return count;
+//    }
     public int hammingWeight(int n) {
-        int count = 0;
-        while(n != 0){
-            if((n & 0x1) == 1){
-                count++;
-            }
-            n >>>= 1;
-        }
-        return count;
+        n = (n & 0x55555555) + ((n >>> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+        n = (n & 0x0f0f0f0f) + ((n >>> 4) & 0x0f0f0f0f);
+        n = (n & 0x00ff00ff) + ((n >>> 8) & 0x00ff00ff);
+        n = (n & 0x0000ffff) + ((n >>> 16) & 0x0000ffff);
+        return n;
     }
+
 }
