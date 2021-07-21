d25733aef0a4446aa17f69385ce59ebc7f13156a
x <522909322@qq.com>
2021-07-21 21:05:12 +0800

    update offer 52

--- src/main/java/easy/Offer52.java
+++ src/main/java/easy/Offer52.java
@@ -1,5 +1,6 @@
 package easy;
 
+
 import list.ListNode;
 
 /**
@@ -7,53 +8,52 @@ import list.ListNode;
  * @version 2020/10/04  18:31
  */
 public class Offer52 {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA == null || headB == null){
-            return  null;
-        }
-        ListNode ta = headA;
-        int la = 0;
-        while(ta != null){
-            la++;
-            ta = ta.next;
-        }
-
-        ListNode tb = headB;
-        int lb = 0;
-        while(tb != null){
-            lb++;
-            tb = tb.next;
-        }
-
-        ta = headA;
-        while(la > lb){
-            ta = ta.next;
-            --la;
-        }
-
-        tb = headB;
-        while(la < lb){
-            tb = tb.next;
-            --lb;
-        }
-
-        while(ta != tb){
-            ta = ta.next;
-            tb = tb.next;
-        }
-        return ta;
-    }
-
+//    2020/10/04  18:31
 //    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-//        if(headA == null || headB == null) return null;
-//        ListNode pa = headA;
-//        ListNode pb = headB;
-//        int count = 0;
-//        while(pa != pb){
-//            count++;
-//            pa = pa == null ? headB : pa.next;
-//            pb = pb == null ? headA : pb.next;
+//        if(headA == null || headB == null){
+//            return  null;
 //        }
-//        return pa;
+//        ListNode ta = headA;
+//        int la = 0;
+//        while(ta != null){
+//            la++;
+//            ta = ta.next;
+//        }
+//
+//        ListNode tb = headB;
+//        int lb = 0;
+//        while(tb != null){
+//            lb++;
+//            tb = tb.next;
+//        }
+//
+//        ta = headA;
+//        while(la > lb){
+//            ta = ta.next;
+//            --la;
+//        }
+//
+//        tb = headB;
+//        while(la < lb){
+//            tb = tb.next;
+//            --lb;
+//        }
+//
+//        while(ta != tb){
+//            ta = ta.next;
+//            tb = tb.next;
+//        }
+//        return ta;
 //    }
+
+    //2021/7/21 21:03
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+        return a;
+    }
 }
