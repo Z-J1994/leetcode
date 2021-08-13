2efa8e450d9a04238c0162e89024f8e927041879
x <522909322@qq.com>
2021-08-13 23:59:40 +0800

    add 233

---
+++ src/main/java/hard/Solution223.java
@@ -0,0 +1,54 @@
+package hard;
+
+/**
+ * @author zhangjun
+ * @version 2021/8/13
+ */
+public class Solution223 {
+    public int countDigitOne(int n) {
+        int count = 0;
+        while(n > 0){
+            int t = get(n);
+            int x = n / t;
+            if(x == 1){
+                int k = n % t + 1;
+                count += k;
+                count += compute(t);
+                n -= t;
+            }else{
+                count += t;
+                count += compute(t) * x;
+                n %= t;
+            }
+        }
+        return count;
+    }
+
+    private int get(int x){
+        int t = 1;
+        while(x >= 10){
+            t *= 10;
+            x /= 10;
+        }
+        return t;
+    }
+
+    private int compute(int x){
+        int count = 0;
+        while(x > 0){
+            count++;
+            x /= 10;
+        }
+        return fastPow(count - 1) / 10 * (count - 1);
+    }
+
+    private int fastPow(int x){
+        int result = 1;
+        for(int i = 10;x > 0;x >>>= 1,i *= i){
+            if((x & 0x1) == 1){
+                result *= i;
+            }
+        }
+        return result;
+    }
+}
