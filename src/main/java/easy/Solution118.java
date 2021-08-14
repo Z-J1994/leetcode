091b1116f5f9faacfff9bfd4e065b55adae183b6
x <522909322@qq.com>
2021-08-14 22:35:28 +0800

    update

--- src/main/java/easy/Solution118.java
+++ src/main/java/easy/Solution118.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Solution118 {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> level = new ArrayList();
+        List<Integer> level = new ArrayList<>();
         level.add(1);
         result.add(level);
         if(numRows == 1){
@@ -19,7 +19,7 @@ public class Solution118 {
         List<Integer> previous;
         for(int i = 1;i < numRows;i++){
             previous = level;
-            level = new ArrayList();
+            level = new ArrayList<>();
             level.add(1);
             for(int j = 1;j < i;j++){
                 level.add(previous.get(j - 1) + previous.get(j));
