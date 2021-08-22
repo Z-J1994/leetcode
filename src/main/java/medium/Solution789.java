e8712ab15fe47861c62aef7836f902ce8308d868
x <522909322@qq.com>
2021-08-22 23:20:19 +0800

    add 789

---
+++ src/main/java/medium/Solution789.java
@@ -0,0 +1,17 @@
+package medium;
+
+/**
+ * @author zhangjun
+ * @version 2021/8/22
+ */
+public class Solution789 {
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        int distance = Math.abs(target[0]) + Math.abs(target[1]);
+        for(int [] ghost :ghosts){
+            if(Math.abs(ghost[0] - target [0]) + Math.abs(ghost[1] - target[1]) <= distance){
+                return false;
+            }
+        }
+        return true;
+    }
+}
