1f26068d7992cc003ab38987fc6e64b653b05c99
章军 <zhangj@isyscore.com>
2021-08-27 14:04:24 +0800

    update 259

--- src/main/java/hard/Solution259.java
+++ src/main/java/hard/Solution259.java
@@ -8,71 +8,69 @@ import java.util.PriorityQueue;
  * @author zhangjun
  * data 2021/08/27 13:43
  */
-public class Solution259{
-    class MedianFinder {
+public class Solution259 {
+    private static class MedianFinder {
 
-        // private Heap max;
-        // private Heap min;
+        private final Heap max;
+        private final Heap min;
 
-        // /** initialize your data structure here. */
-        // public MedianFinder() {
-        //      max = new Heap(true);
-        //      min = new Heap(false);
-        // }
-        PriorityQueue<Integer> min;
-        PriorityQueue<Integer> max;
-
+        /**
+         * initialize your data structure here.
+         */
         public MedianFinder() {
-            max = new PriorityQueue<Integer>((a, b) -> (b - a));
-            min = new PriorityQueue<Integer>((a, b) -> (a - b));
+            max = new Heap(true);
+            min = new Heap(false);
         }
+
         int count;
+
         public void addNum(int num) {
-            if(count == 0){
+            if (count == 0) {
                 max.offer(num);
-            }else if(num >= max.peek()){
+            } else if (num >= max.peek()) {
                 min.offer(num);
-            }else{
+            } else {
                 max.offer(num);
             }
             count++;
-            if(min.size() > max.size()){
+            if (min.size() > max.size()) {
                 max.offer(min.poll());
-            }else if(max.size() - min.size() > 1){
+            } else if (max.size() - min.size() > 1) {
                 min.offer(max.poll());
             }
         }
 
         public double findMedian() {
-            if((count & 0x1) == 0){
+            if ((count & 0x1) == 0) {
                 return (max.peek() * 1.0 + min.peek()) / 2;
             }
             return max.peek();
         }
 
-        private static class Heap{
-            int [] heap;
+        private static class Heap {
+            int[] heap;
             int size;
             int factory = -1;
-            public Heap(boolean max){
-                if(max){
+
+            public Heap(boolean max) {
+                if (max) {
                     factory = 1;
                 }
                 heap = new int[16];
             }
 
-            private boolean compare(int a,int b){
+            private boolean compare(int a, int b) {
                 return (a - b) * factory > 0;
             }
 
-            private void sink(int parent){
+            private void sink(int parent) {
                 int child = parent * 2 + 1;
                 int parentValue = heap[parent];
-                while(child < size){
-                    if(child + 1 < size && compare(heap[child + 1],heap[child])){
+                while (child < size) {
+                    if (child + 1 < size && compare(heap[child + 1], heap[child])) {
                         child++;
                     }
-                    if(compare(parentValue,heap[child])){
+                    if (compare(parentValue, heap[child])) {
                         break;
                     }
                     heap[parent] = heap[child];
@@ -82,10 +80,10 @@ public class Solution259{
                 heap[parent] = parentValue;
             }
 
-            private void swim(int child){
+            private void swim(int child) {
                 int childValue = heap[child];
                 int parent = (child - 1) >>> 1;
-                while(child > 0 && compare(childValue,heap[parent])){
+                while (child > 0 && compare(childValue, heap[parent])) {
                     heap[child] = heap[parent];
                     child = parent;
                     parent = (child - 1) >>> 1;
@@ -93,37 +91,37 @@ public class Solution259{
                 heap[child] = childValue;
             }
 
-            public void offer(int element){
-                if(size == heap.length){
+            public void offer(int element) {
+                if (size == heap.length) {
                     grow(size << 1);
                 }
                 heap[size++] = element;
                 swim(size - 1);
             }
 
-            public int poll(){
+            public int poll() {
                 int result = heap[0];
                 heap[0] = heap[--size];
                 sink(0);
                 return result;
             }
 
-            public int peek(){
+            public int peek() {
                 return heap[0];
             }
 
-            public void setTop(int element){
+            public void setTop(int element) {
                 heap[0] = element;
                 sink(0);
             }
 
-            public int size(){
+            public int size() {
                 return size;
             }
 
-            private void grow(int newCapacity){
-                int [] newHeap = new int[newCapacity];
-                for(int i = 0,l = heap.length;i < l;i++){
+            private void grow(int newCapacity) {
+                int[] newHeap = new int[newCapacity];
+                for (int i = 0, l = heap.length; i < l; i++) {
                     newHeap[i] = heap[i];
                 }
                 heap = newHeap;
