459dc86719dbf3de3c2f17dd05ba718921d7d36e
x <522909322@qq.com>
2021-09-07 22:50:21 +0800

    update 1221

--- src/main/java/easy/Solution1221.java
+++ src/main/java/easy/Solution1221.java
@@ -7,27 +7,18 @@ package easy;
 public class Solution1221{
 
     public int balancedStringSplit(String s) {
-        char [] chars = s.toCharArray();
         int count = 0;
-        int l = 0;
-        int r = 0;
-        for(int i = 0;i < chars.length;i++){
-            if(chars[i] == 'R'){
-                r++;
-            } else if(chars[i] == 'L'){
-                l++;
+        int c = 0;
+        for(int i = 0,l = s.length();i < l;i++){
+            if(s.charAt(i) == 'L'){
+                c++;
+            }else{
+                c--;
             }
-            if(l == r){
+            if(c == 0){
                 count++;
-                l = 0;
-                r = 0;
             }
         }
-
-        if(l != r){
-            return 0;
-        }
-
         return count;
     }
 
