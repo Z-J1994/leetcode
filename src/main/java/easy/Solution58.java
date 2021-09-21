ee35769517ba94e6e48e58ca0218037c615b24ac
x <522909322@qq.com>
2021-09-21 13:01:17 +0800

    add 58

---
+++ src/main/java/easy/Solution58.java
@@ -0,0 +1,21 @@
+package easy;
+
+/**
+ * @author zhangjun
+ * @version 2021/9/21
+ */
+public class Solution58 {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        for(int i = s.length() - 1;i >= 0;i--){
+            if(s.charAt(i) == ' '){
+                if(count != 0){
+                    break;
+                }
+            }else{
+                count++;
+            }
+        }
+        return count;
+    }
+}
