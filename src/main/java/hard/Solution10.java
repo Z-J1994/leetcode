a8576c38f0720b514baa8b8793d0e7c8ac0778d7
x <522909322@qq.com>
2021-09-22 21:28:16 +0800

    add 10

--- src/main/java/hard/Solution10.java
+++ src/main/java/hard/Solution10.java
@@ -6,12 +6,17 @@ package hard;
  */
 public class Solution10 {
     public boolean isMatch(String s, String p) {
-        return dfs(s.toCharArray(),p.toCharArray(),0,0);
+
+        return dfs(s.toCharArray(),p.toCharArray(),0,0,new Boolean[s.length() + 1][p.length() + 1]);
     }
 
-    private boolean dfs(char [] s,char [] p,int indexS,int indexP){
+    private boolean dfs(char [] s,char [] p,int indexS,int indexP,Boolean [][] map){
         if(indexP == p.length){
             return indexS == s.length;
+        }
+
+        if(map[indexS][indexP] != null){
+            return map[indexS][indexP];
         }
 
         //单个匹配
@@ -19,10 +24,11 @@ public class Solution10 {
 
         //通配,匹配或者放弃匹配
         if(indexP + 1 < p.length && p[indexP + 1] == '*'){
-            result = (result && dfs(s,p,indexS + 1,indexP)) || dfs(s,p,indexS,indexP + 2);
+            result = (result && dfs(s,p,indexS + 1,indexP,map)) || dfs(s,p,indexS,indexP + 2,map);
         }else{
-            result = result && dfs(s,p,indexS + 1,indexP + 1);
+            result = result && dfs(s,p,indexS + 1,indexP + 1,map);
         }
+        map[indexS][indexP] = result;
         return result;
     }
 }
