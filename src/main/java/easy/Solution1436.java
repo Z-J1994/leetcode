d3f65a1838644d53484adbcf5d7a8b0651caffc4
x <522909322@qq.com>
2021-10-01 09:51:49 +0800

    update 1436

--- src/main/java/easy/Solution1436.java
+++ src/main/java/easy/Solution1436.java
@@ -9,22 +9,39 @@ import java.util.Map;
  * @version 2020/09/22  19:27
  */
 public class Solution1436 {
+//    public String destCity(List<List<String>> paths) {
+//        Map<String,Integer> map = new HashMap<>();
+//        for(List<String> list : paths){
+//            String source = list.get(0);
+//            String distance = list.get(1);
+//            map.put(source,1);
+//            if(!map.containsKey(distance)){
+//                map.put(distance,0);
+//            }
+//        }
+//        String key = "";
+//        for(Map.Entry<String,Integer> entry: map.entrySet()){
+//            if(entry.getValue() == 0){
+//               key =  entry.getKey();
+//            }
+//        }
+//        return key;
+//    }
+
+    //2021/10/1二刷
     public String destCity(List<List<String>> paths) {
-        Map<String,Integer> map = new HashMap<>();
+        Map<String,String> map = new HashMap<>();
         for(List<String> list : paths){
-            String source = list.get(0);
-            String distance = list.get(1);
-            map.put(source,1);
-            if(!map.containsKey(distance)){
-                map.put(distance,0);
-            }
+            map.put(list.get(0),list.get(1));
         }
-        String key = "";
-        for(Map.Entry<String,Integer> entry: map.entrySet()){
-            if(entry.getValue() == 0){
-               key =  entry.getKey();
-            }
+
+        String next = paths.get(0).get(0);
+        String result = null;
+        while(next != null){
+            result = next;
+            next = map.get(next);
         }
-        return key;
+        return result;
     }
+
 }
