de9bbe08f8457e0ec54c565469b2723d1d4c3a23
zj <522909322@qq.com>
2021-10-22 20:54:18 +0800

    add 229

---
+++ src/main/java/medium/Solution229.java
@@ -0,0 +1,30 @@
+package medium;
+
+public class Solution229 {
+    public List<Integer> majorityElement(int[] nums) {
+        Map<Integer,ValueHolder> map = new HashMap<>();
+        for(int n : nums){
+            ValueHolder count = map.get(n);
+            if(count == null){
+                count = new ValueHolder(0);
+                map.put(n,count);
+            }
+            count.value++;
+        }
+        int limit = nums.length / 3;
+        List<Integer> result = new ArrayList<>();
+        for(Map.Entry<Integer,ValueHolder> entry : map.entrySet()){
+            if(entry.getValue().value > limit){
+                result.add(entry.getKey());
+            }
+        }
+        return result;
+    }
+
+    private static class ValueHolder{
+        private int value;
+        private ValueHolder(int value){
+            this.value = value;
+        }
+    }
+}
