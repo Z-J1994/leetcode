2778d8c6cc9dc3794cdb517537ea5b5f642fbf87
Z-J1994 <522909322@qq.com>
2021-11-01 21:18:42 +0800

    add 575

---
+++ src/main/java/easy/Solution575.java
@@ -0,0 +1,14 @@
+package easy;
+
+public class Solution575 {
+    public int distributeCandies(int[] candyType) {
+        byte [] map = new byte[200001];
+        int typeCount = 0;
+        for(int i = 0;i < candyType.length;i++){
+            typeCount += (map[candyType[i] + 100000] ^ 1);
+            map[candyType[i] + 100000] = 1;
+        }
+        int half = candyType.length / 2;
+        return half >= typeCount ? typeCount : half;
+    }
+}
