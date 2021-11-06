5e8865bee5cdb299dd4683e5686ff66d187325a6
Z-J1994 <522909322@qq.com>
2021-11-06 16:35:37 +0800

    add 268

--- src/main/java/easy/Solution268.java
+++ src/main/java/easy/Solution268.java
@@ -5,12 +5,21 @@ package easy;
  * @version 2020/10/07  22:13
  */
 public class Solution268 {
-    public int missingNumber(int[] nums) {
-        int length = nums.length;
-        int result = (1 + length) * length / 2;
+//    public int missingNumber(int[] nums) {
+//        int length = nums.length;
+//        int result = (1 + length) * length / 2;
+//
+//        for(int i = 0;i < length;i++){
+//            result -= nums[i];
+//        }
+//        return result;
+//    }
 
-        for(int i = 0;i < length;i++){
-            result -= nums[i];
+    public int missingNumber(int[] nums) {
+//        int result = 0 ^ nums.length;
+        int result = nums.length;
+        for(int i = 0;i < nums.length;i++){
+            result = result ^ nums[i] ^ i;
         }
         return result;
     }
