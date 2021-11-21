cc0f6002e07371904992ec65b6f873438257bff2
Z-J1994 <522909322@qq.com>
2021-11-21 13:38:03 +0800

    update 559

--- src/main/java/easy/Solution559.java
+++ src/main/java/easy/Solution559.java
@@ -7,11 +7,12 @@ import java.util.List;
  * @version 2020/09/28  16:36
  */
 public class Solution559 {
-    private class Node {
+    private static class Node {
         public int val;
         public List<Node> children;
 
-        public Node() {}
+        public Node() {
+        }
 
         public Node(int _val) {
             val = _val;
@@ -23,23 +24,44 @@ public class Solution559 {
         }
     }
 
-    int depth = 0;
+//    int depth = 0;
+//
+//    public int maxDepth(Node root) {
+//        recursion(root,0);
+//        return depth;
+//    }
+//
+//    private void recursion(Node root,int depth){
+//        if(root != null){
+//            if(depth > this.depth){
+//                this.depth = depth;
+//            }
+//            if(root.children != null){
+//                for(Node n : root.children){
+//                    recursion(n,depth + 1);
+//                }
+//            }
+//        }
+//    }
 
     public int maxDepth(Node root) {
-        recursion(root,0);
-        return depth;
+        if (root != null) {
+            dfs(root, 1);
+        }
+        return max;
     }
 
-    private void recursion(Node root,int depth){
-        if(root != null){
-            if(depth > this.depth){
-                this.depth = depth;
+    int max = 0;
+
+    private void dfs(Node root, int depth) {
+        if (root == null || root.children == null || root.children.isEmpty()) {
+            if (depth > max) {
+                max = depth;
             }
-            if(root.children != null){
-                for(Node n : root.children){
-                    recursion(n,depth + 1);
-                }
-            }
+            return;
+        }
+        for (Node child : root.children) {
+            dfs(child, depth + 1);
         }
     }
 
