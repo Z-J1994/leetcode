bfb1536a2581c011488d066c1b226afc9634ff26
Z-J1994 <522909322@qq.com>
2021-12-04 20:46:02 +0800

    add 383

---
+++ src/main/java/easy/Solution383.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class Solution383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] alphabet = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            alphabet[magazine.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (--alphabet[ransomNote.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
