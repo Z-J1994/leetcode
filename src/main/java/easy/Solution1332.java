bc53bd3ddcb563e039eb36e312980dbbdb6fa886
Z-J1994 <522909322@qq.com>
2022-01-22 19:49:34 +0800

    update 1332

--- src/main/java/easy/Solution1332.java
+++ src/main/java/easy/Solution1332.java
@@ -6,11 +6,8 @@ package easy;
  */
 public class Solution1332 {
     public int removePalindromeSub(String s) {
-        if(s.length() == 0){
-            return 0;
-        }
-        for(int i = 0,j = j = s.length() - 1;i < j;i++,j--){
-            if(s.charAt(i) != s.charAt(j)){
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
                 return 2;
             }
         }
