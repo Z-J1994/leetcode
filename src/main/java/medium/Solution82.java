2762c7c67f94078ed5680d2dea24fa8c613328cc
x <522909322@qq.com>
2022-02-10 16:52:15 +0800

    update 82

--- src/main/java/medium/Solution82.java
+++ src/main/java/medium/Solution82.java
@@ -8,23 +8,17 @@ import list.ListNode;
  */
 public class Solution82 {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head == null){
-            return null;
-        }
-        head = new ListNode(1,head);
-        ListNode p = head;
-        while(p.next != null){
-            int value = p.next.val;
-            ListNode next = p.next.next;
-            int count = 0;
-            while(next != null && next.val == value){
-                next = next.next;
-                count++;
+        head = new ListNode(-101,head);
+        ListNode current = head;
+        while(current.next != null){
+            ListNode n = current.next.next;
+            while(n != null && current.next.val == n.val){
+                n = n.next;
             }
-            if(count != 0){
-                p.next = next;
+            if(current.next.next != n){
+                current.next = n;
             }else{
-                p = p.next;
+                current = current.next;
             }
         }
         return head.next;
