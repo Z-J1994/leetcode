e4d00988eccbf29bd9d4f714e069073998b03e7f
Z-J1994 <522909322@qq.com>
2022-05-21 15:30:46 +0800

    update 961

--- src/main/java/easy/Solution961.java
+++ src/main/java/easy/Solution961.java
@@ -2,26 +2,16 @@ package easy;
 
 public class Solution961 {
     public int repeatedNTimes(int[] nums) {
-        int t = nums[0];
-        int c = 1;
-        for(int i = 1;i < nums.length;i++){
-            if(nums[i] == t){
-                if(++c > 1){
-                    return t;
-                }
-            }else {
-                if(--c < 0){
-                    t = nums[i];
-                    c = 1;
-                }
+        if(nums[0] == nums[1]){
+            return nums[1];
+        }
+        for(int i = 2;i < nums.length;i++){
+            //一共有n+1个元素,当2n > 4时必定有两个元素相同的距离不超过2
+            if(nums[i] == nums[i - 1] || nums[i] == nums[i - 2]){
+                return nums[i];
             }
         }
-        t = nums[nums.length - 1];
-        for(int i = nums.length - 2;i >= 0;i--){
-            if(nums[i] == t){
-                return t;
-            }
-        }
-        return nums[nums.length - 2];
+        //当2n == 4 时要考虑[a,b,c,a]
+        return nums[0];
     }
 }
