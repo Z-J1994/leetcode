0faf378ab353814c7f73867e3db46e6d4fc43bfc
Z-J1994 <522909322@qq.com>
2022-05-28 18:34:36 +0800

    update 1021

--- src/main/java/easy/Solution1021.java
+++ src/main/java/easy/Solution1021.java
@@ -5,29 +5,21 @@ package easy;
  * @version 2020/09/22  21:12
  */
 public class Solution1021 {
-
-    public String removeOuterParentheses(String S) {
-        int l = 0;
-        int r = 0;
-        int mark = 0;
-        char [] chars = S.toCharArray();
-        StringBuilder result = new StringBuilder();
-        for(int i = 0;i < S.length();i++){
-            char c = chars[i];
-            if(c == '('){
-                l++;
-            }else {
-                r++;
-                if(l == r){
-                    result.append(chars,mark + 1,l + r - 2);
-                    l = 0;
-                    r = 0;
-                    mark = i + 1;
+    public String removeOuterParentheses(String s) {
+        int length = s.length();
+        char [] chars = new char[length];
+        int index = 0;
+        for(int i = 0,leftCount = 0;i < length;i++){
+            if(s.charAt(i) == '('){
+                if(leftCount++ != 0){
+                    chars[index++] = '(';
+                }
+            }else{
+                if(--leftCount != 0){
+                    chars[index++] = ')';
                 }
             }
         }
-        return result.toString();
+        return new String(chars,0,index);
     }
-
-
 }
