848216c1050211a420d0b392979adf5d2ff665bd
章军 <522909322@qq.com>
2022-06-08 12:33:39 +0800

    update 1039

--- src/main/java/easy/Solution1037.java
+++ src/main/java/easy/Solution1037.java
@@ -8,11 +8,12 @@ import utils.Parser;
  */
 public class Solution1037 {
     public boolean isBoomerang(int[][] points) {
+        //判断斜率 (y2 - y1)/(x2 - x1) != (y3 - y1)/(x3 - x1) ==> (y2 - y1) * (x3 - x1) != (x2 - x1) * (y3 - y1)
         return (points[1][1] - points[0][1]) * (points[2][0] - points[0][0]) != (points[1][0] - points[0][0]) * (points[2][1] - points[0][1]);
     }
 
     public static void main(String[] args) {
-        Solution1037 s  = new Solution1037();
+        Solution1037 s = new Solution1037();
         System.out.println(s.isBoomerang(Parser.stringToIntMatrix("[[73,31],[73,19],[73,45]]", 2)));
     }
 }
