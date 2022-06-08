e85690b083415cbc957fcdbe36b7627f9b085c02
章军 <522909322@qq.com>
2022-06-08 12:31:26 +0800

    update 1039

--- src/main/java/easy/Solution1037.java
+++ src/main/java/easy/Solution1037.java
@@ -8,14 +8,6 @@ import utils.Parser;
  */
 public class Solution1037 {
     public boolean isBoomerang(int[][] points) {
-        for(int i = 0;i <= 3;i++){
-            if(points[i % 3][0] == points[(i + 1) % 3][0] && points[i % 3][1] == points[(i + 1) % 3][1]){
-                return false;
-            }
-        }
-        if(points[0][0] == points[1][0] ^ points[0][0] == points[2][0]){
-            return true;
-        }
         return (points[1][1] - points[0][1]) * (points[2][0] - points[0][0]) != (points[1][0] - points[0][0]) * (points[2][1] - points[0][1]);
     }
 
