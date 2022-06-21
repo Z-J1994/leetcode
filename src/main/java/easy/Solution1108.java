ce088c2aa97d8bd7371d9aa404cb3bcf8fb39e75
章军 <522909322@qq.com>
2022-06-21 12:55:11 +0800

    update 1108

--- src/main/java/easy/Solution1108.java
+++ src/main/java/easy/Solution1108.java
@@ -6,6 +6,18 @@ package easy;
  */
 public class Solution1108 {
     public String defangIPaddr(String address) {
-        return address.replace(".","[.]");
+        int length = address.length();
+        char [] chars = new char[length + 6];
+        for(int i = 0,j = 0;i < length;i++){
+            char c = address.charAt(i);
+            if(c == '.'){
+                chars[j++] = '[';
+                chars[j++] = '.';
+                chars[j++] = ']';
+            }else{
+                chars[j++] = c;
+            }
+        }
+        return new String(chars);
     }
 }
