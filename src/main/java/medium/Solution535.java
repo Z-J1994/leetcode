254a10e147825f5147a513485fe293086a260bf5
章军 <522909322@qq.com>
2022-06-29 12:47:42 +0800

    add 535

---
+++ src/main/java/medium/Solution535.java
@@ -0,0 +1,17 @@
+package medium;
+
+/**
+ * @author zhangjun
+ * @since 2022/6/29 12:47
+ */
+public class Solution535 {
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return shortUrl;
+    }
+}
