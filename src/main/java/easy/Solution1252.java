d2763360b3c636e81603d54cc7d5ee67118f7b01
章军 <522909322@qq.com>
2022-07-12 12:36:16 +0800

    update 1252 add 1394

--- src/main/java/easy/Solution1252.java
+++ src/main/java/easy/Solution1252.java
@@ -5,26 +5,21 @@ package easy;
  * @version 2020/09/23  14:47
  */
 public class Solution1252 {
-    public int oddCells(int n, int m, int[][] indices) {
-        int [][] matrix = new int[n][m];
-        for(int [] arrays : indices){
-            int row = arrays[0];
-            for(int k = 0;k < m;k++){
-                matrix[row][k]++;
-            }
-
-            int column = arrays[1];
-            for(int k = 0;k < n;k++){
-                matrix[k][column]++;
-            }
+    public int oddCells(int m, int n, int[][] indices) {
+        int[] am = new int[m];
+        int[] an = new int[n];
+        for (int[] i : indices) {
+            am[i[0]] += 1;
+            an[i[1]] += 1;
+        }
+        int[] element = new int[2];
+        for (int i = 0; i < m; i++) {
+            element[0] += am[i] & 1;
+            element[1] += 1 - (am[i] & 1);
         }
         int count = 0;
-        for(int [] arrays : matrix){
-            for(int i:arrays){
-                if(i % 2 == 1){
-                    count++;
-                }
-            }
+        for (int i = 0; i < n; i++) {
+            count += element[an[i] & 1];
         }
         return count;
     }
