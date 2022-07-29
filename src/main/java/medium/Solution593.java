855b35e733e1e11c47331d0c364896b22c790967
Z-J1994 <522909322@qq.com>
2022-07-30 02:31:16 +0800

    update 593

--- src/main/java/medium/Solution593.java
+++ src/main/java/medium/Solution593.java
@@ -11,29 +11,18 @@ public class Solution593 {
             }
             return b[1] - a[1];
         });
-        for(int i = 1;i < 4;i++){
-            if(points[i][0] == points[i - 1][0] && points[i][1] == points[i - 1][1]){
-                return false;
-            }
-        }
+
         if (check(points[0], points[1], points[2]) && check(points[3], points[2], points[1])) {
             double d1 = Math.pow((points[2][1] - points[0][1]), 2) + Math.pow((points[2][0] - points[0][0]), 2);
             double d2 = Math.pow((points[1][1] - points[0][1]), 2) + Math.pow((points[1][0] - points[0][0]), 2);
             double d3 = Math.pow((points[2][1] - points[3][1]), 2) + Math.pow((points[2][0] - points[3][0]), 2);
             double d4 = Math.pow((points[1][1] - points[3][1]), 2) + Math.pow((points[1][0] - points[3][0]), 2);
-            return d1 == d2 && d1 == d3 && d1 == d4;
+            return d1 == d2 && d1 == d3 && d1 == d4 && d1 != 0;
         }
         return false;
     }
 
-    private boolean check(int[] p1, int[] p2, int[] p3) {
+    private boolean check(int[] p1, int[] p2, int[] p3){
         return (p3[1] - p1[1]) * (p2[1] - p1[1]) == -(p3[0] - p1[0]) * (p2[0] - p1[0]);
-    }
-
-    public static void main(String[] args) {
-        Solution593 s = new Solution593();
-//        System.out.println(s.validSquare(new int[]{0, 0}, new int[]{1, 1}, new int[]{1, 0}, new int[]{0, 1}));
-
-        System.out.println(s.validSquare(new int[]{0, 0}, new int[]{5, 0}, new int[]{5, 4}, new int[]{0, 4}));
     }
 }
