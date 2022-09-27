9a45df10bd8dcce4892a494378afc60249f9fe63
章军 <522909322@qq.com>
2022-09-27 12:45:52 +0800

    update interview01_02

--- src/main/java/easy/Interview01_02.java
+++ src/main/java/easy/Interview01_02.java
@@ -6,25 +6,18 @@ package easy;
  */
 public class Interview01_02 {
     public boolean CheckPermutation(String s1, String s2) {
-        int length1 = s1.length();
-        int length2 = s2.length();
-        if(length1 != length2){
+        if(s1.length() != s2.length()){
             return false;
         }
-        //假定字符串中只有英文字母。
-        byte [] map1 = new byte[123];
-        byte [] map2 = new byte[123];
-        for(int i = 0;i < length1;i++){
-            map1[s1.charAt(i)]++;
-            map2[s2.charAt(i)]++;
+        int [] alphabet = new int[128];
+        for(int i = 0,length = s1.length();i < length;i++){
+            alphabet[s1.charAt(i)]++;
         }
-
-        for(int i = 0;i < 123;i++){
-            if(map1[i] != map2[i]){
+        for(int i = 0,length = s2.length();i < length;i++){
+            if(--alphabet[s2.charAt(i)] < 0){
                 return false;
             }
         }
         return true;
     }
-
 }
