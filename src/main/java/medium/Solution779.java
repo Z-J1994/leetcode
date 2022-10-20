37a2d1a873c1a0a9e149d18372130f8846d8567e
章军 <522909322@qq.com>
2022-10-20 21:49:58 +0800

    update 779

--- src/main/java/medium/Solution779.java
+++ src/main/java/medium/Solution779.java
@@ -2,21 +2,12 @@ package medium;
 
 public class Solution779 {
     public int kthGrammar(int n, int k) {
-        int c = 0;
-        while(k != 1){
-            c++;
-            k -= get(k);
-        }
-        return c & 1;
-    }
-
-    private int get(int n){
-        n -= 1;
-        n |= n >> 1;
-        n |= n >> 2;
-        n |= n >> 4;
-        n |= n >> 8;
-        n |= n >> 16;
-        return (n + 1) >> 1;
+        k -= 1;
+        k = (k & 0x55555555) + ((k >> 1) & 0x55555555);
+        k = (k & 0x33333333) + ((k >> 2) & 0x33333333);
+        k = (k & 0x0F0F0F0F) + ((k >> 4) & 0x0F0F0F0F);
+        k = (k & 0x00FF00FF) + ((k >> 8) & 0x00FF00FF);
+        k = (k & 0x0000FFFF) + ((k >> 16) & 0x0000FFFF);
+        return k & 1;
     }
 }
