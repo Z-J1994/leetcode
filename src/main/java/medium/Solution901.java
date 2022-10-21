3dbc70cb040343be97c0c748db6f63db101093de
章军 <522909322@qq.com>
2022-10-21 14:06:40 +0800

    update 901

--- src/main/java/medium/Solution901.java
+++ src/main/java/medium/Solution901.java
@@ -1,26 +1,41 @@
 package medium;
 
+import java.util.Arrays;
+
 /**
  * @author zhangjun
  * @version 2022/2/10
  */
 public class Solution901 {
     private static class StockSpanner {
-        private final int[][] stack;
-        int top = -1;
+        private int[][] element;
+        private int index;
+
         public StockSpanner() {
-            stack = new int[2][10001];
+            element = new int[16][2];
+            index = 0;
         }
 
         public int next(int price) {
-            int sum = 1;
-            while(top >= 0 && stack[0][top] <= price){
-                sum += stack[1][top];
-                --top;
+            int count = 1;
+            while (index >= 0 && element[index][0] <= price) {
+                count += element[index][1];
+                index--;
             }
-            stack[0][++top] = price;
-            stack[1][top] = sum;
-            return sum;
+            if(index == element.length - 1){
+                element = Arrays.copyOf(element,element.length << 1);
+            }
+            element[++index] = new int[]{price,count};
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        StockSpanner stockSpanner = new StockSpanner();
+//        int [] e = {100,80,60,70,60,75,85};
+        int[] e = {9, 10, 11, 12, 28, 40, 65, 86, 88, 89};
+        for (int i : e) {
+            System.out.println(stockSpanner.next(i));
         }
     }
 }
