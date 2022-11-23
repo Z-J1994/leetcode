e04df563619197cb092a2afa1aff9e8cffe44030
章军 <522909322@qq.com>
2022-11-23 13:21:39 +0800

    add 1742

---
+++ src/main/java/easy/Solution1742.java
@@ -0,0 +1,35 @@
+package easy;
+
+/**
+ * @author zhangjun
+ * @version 2022/11/23
+ */
+public class Solution1742 {
+    public int countBalls(int lowLimit, int highLimit) {
+        int c = 0;
+        for (int i = highLimit; i > 0; i /= 10, c++) ;
+        int[] map = new int[(c + 1) * 10];
+        while (lowLimit <= highLimit) {
+            int t = bitSum(lowLimit);
+            for (int j = lowLimit % 10; j < 10 && lowLimit <= highLimit; j++, lowLimit++) {
+                map[t++]++;
+            }
+        }
+        int max = 0;
+        for (int j : map) {
+            if (j > max) {
+                max = j;
+            }
+        }
+        return max;
+    }
+
+    private int bitSum(int n) {
+        int s = 0;
+        while (n > 0) {
+            s += n % 10;
+            n /= 10;
+        }
+        return s;
+    }
+}
