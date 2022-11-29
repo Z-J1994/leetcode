b7d87e3882940f1732618538a7365ddf863460eb
章军 <522909322@qq.com>
2022-11-29 12:56:52 +0800

    update 1758

--- src/main/java/easy/Solution1758.java
+++ src/main/java/easy/Solution1758.java
@@ -7,12 +7,9 @@ package easy;
 public class Solution1758 {
     public int minOperations(String s) {
         int a = 0;
-        int b = 0;
-        for(int i = 0,l = s.length(),c;i < l;i++){
-            c = (s.charAt(i) - '0') ^ (i & 1);
-            a += c;
-            b += 1 - c;
+        for(int i = 0,l = s.length();i < l;i++){
+            a += (s.charAt(i) - '0') ^ (i & 1);
         }
-        return Math.min(a,b);
+        return Math.min(a,s.length() - a);
     }
 }
