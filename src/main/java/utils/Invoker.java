2647eca51865d5c79e3349f1374e34271709d8d1
章军 <522909322@qq.com>
2023-11-28 15:10:29 +0800

    新增工具类

---
+++ src/main/java/utils/Invoker.java
@@ -0,0 +1,53 @@
+package utils;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author zhangjun
+ * @since 2023/11/28 14:08
+ */
+public class Invoker {
+
+    public static <T> T[] invoke(Class<?> target, Object[] args, T[] results, String[] methodNames) {
+        Map<String, Method> methodMap = Arrays.stream(target.getDeclaredMethods()).collect(Collectors.toMap(Method::getName, m -> m));
+        Constructor<?> constructor = target.getDeclaredConstructors()[0];
+        constructor.setAccessible(true);
+        try {
+            Object instance;
+            Class<?> enclosingClass = target.getEnclosingClass();
+            if (enclosingClass != null) {
+                Constructor<?> enclosingClassConstructor = enclosingClass.getDeclaredConstructors()[0];
+                enclosingClassConstructor.setAccessible(true);
+                Object outer = enclosingClassConstructor.newInstance();
+                instance = constructor.newInstance(outer);
+            } else {
+                instance = constructor.newInstance();
+            }
+            for (int i = 1; i < methodNames.length; i++) {
+                Method method = methodMap.get(methodNames[i]);
+                method.setAccessible(true);
+                if (method.getReturnType() == Void.TYPE) {
+                    invoke(instance, method, args[i]);
+                    continue;
+                }
+                results[i] = (T) invoke(instance,method, args[i]);
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+        return results;
+    }
+
+    private static Object invoke(Object instance, Method method, Object args) throws Exception{
+        if(method.getParameterCount() > 0){
+            return method.invoke(instance,  args);
+        }
+        return method.invoke(instance);
+    }
+}
+
+
