02ea73a424bc838de277df08d9cdcb72d6416f66
章军 <522909322@qq.com>
2023-12-04 20:15:57 +0800

    add 1038

---
+++ src/main/java/medium/Solution1038.java
@@ -0,0 +1,17 @@
+package medium;
+
+import tree.TreeNode;
+
+public class Solution1038 {
+    int sum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        bstToGst(root.right);
+        sum += root.val;
+        root.val = sum;
+        bstToGst(root.left);
+        return root;
+    }
+}
