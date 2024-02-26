304734b40f2369077fba9c86874ed3893c058c82
章军 <522909322@qq.com>
2024-02-26 21:42:41 +0800

    update 938

--- src/main/java/easy/Solution938.java
+++ src/main/java/easy/Solution938.java
@@ -1,36 +1,23 @@
 package easy;
 
+import tree.TreeNode;
+
 /**
  * @author zhangjun
  * @version 2020/09/22  22:37
  */
 public class Solution938 {
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int x) { val = x; }
-    }
-    int sum = 0;
-    public int rangeSumBST(TreeNode root, int L, int R) {
-        recursion(root, L, R);
-        return sum;
-    }
-
-    private void recursion(TreeNode root, int L, int R){
-        if(root != null){
-            if(root.val < L){
-                recursion(root.right,L,R);
-                return;
-            }
-            if(root.val > R){
-                recursion(root.left,L,R);
-                return;
-            }
-            sum += root.val;
-            recursion(root.right,L,R);
-            recursion(root.left,L,R);
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root == null){
+            return 0;
+        }
+        int s = 0;
+        if(root.val >= low && root.val <= high){
+            return rangeSumBST(root.left,low,high) + rangeSumBST(root.right,low,high) + root.val;
+        }else if(root.val < low){
+            return rangeSumBST(root.right,low,high);
+        }else {
+            return rangeSumBST(root.left,low,high);
         }
     }
-
 }
