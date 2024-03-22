0d6d5b7e1afc1cb673d777fe114c3cba72bc82ac
章军 <522909322@qq.com>
2024-03-22 13:57:35 +0800

    update 234

--- src/main/java/easy/Solution234.java
+++ src/main/java/easy/Solution234.java
@@ -2,35 +2,50 @@ package easy;
 
 import list.ListNode;
 
+import java.util.List;
+
 /**
  * @author zhangjun
  * @version 2020/10/04  20:32
  */
 public class Solution234 {
     public boolean isPalindrome(ListNode head) {
+        if (head.next == null) {
+            return true;
+        }
         int length = 0;
-        ListNode t = head;
-        while(t != null){
-            length++;
-            t = t.next;
+        ListNode slow = head, sp = new ListNode();
+        for (ListNode fast = head; fast != null; length++) {
+            fast = fast.next;
+            if ((length & 1) == 0) {
+                ListNode p = sp;
+                sp = slow;
+                slow = slow.next;
+                sp.next = p;
+            }
         }
-        ListNode previous = null;
-        for(int i = 0;i < length / 2;i++){
-            t = head;
-            head = head.next;
-            t.next = previous;
-            previous = t;
+        if ((length & 1) == 1) {
+            sp = sp.next;
         }
-        if ((length & 0x1) == 1){
-            head = head.next;
-        }
-        while(previous != null){
-            if(previous.val != head.val){
+
+        while (slow != null) {
+            if (sp.val != slow.val) {
                 return false;
             }
-            previous = previous.next;
-            head = head.next;
+            sp = sp.next;
+            slow = slow.next;
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        Solution234 s = new Solution234();
+        ListNode l = new ListNode(1,new ListNode(2));
+        System.out.println(s.isPalindrome(l) == false);
+
+         l = new ListNode(1,new ListNode(2,new ListNode(2,new ListNode(1))));
+        System.out.println(s.isPalindrome(l) == true);
+        l = new ListNode(1,new ListNode(2,new ListNode(2,new ListNode(2,new ListNode(1)))));
+        System.out.println(s.isPalindrome(l) == true);
     }
 }
