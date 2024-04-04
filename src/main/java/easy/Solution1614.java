360bba9877ca2ec35dd2c9f1ef69d1e385b165ee
章军 <522909322@qq.com>
2024-04-04 23:05:46 +0800

    update 1614

--- src/main/java/easy/Solution1614.java
+++ src/main/java/easy/Solution1614.java
@@ -6,14 +6,15 @@ package easy;
  */
 public class Solution1614 {
     public int maxDepth(String s) {
-        int top = 0;
         int max = 0;
-        for(int i = 0 ,l = s.length();i < l;i++){
+        for(int i = 0,j = 0;i < s.length();i++){
             char c = s.charAt(i);
             if(c == '('){
-                max = ++top > max ? top : max;
-            }else if(c == ')'){
-                --top;
+                if(++j > max){
+                    max = j;
+                }
+            }else if(c ==')'){
+                --j;
             }
         }
         return max;
